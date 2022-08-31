a6ff3df791ae13ae175178ff78144c250561069e
Disha_S <Disha_S@dell.com>
2022-08-31 22:58:20 +0530

    problem  solutions

--- ConsoleApp1/SDE Sheet Code Snippet.cs
+++ ConsoleApp1/SDE Sheet Code Snippet.cs
@@ -294,5 +294,160 @@ namespace ArrayExcersize
             }
             return x + y;
         }
+
+        /* Rotate Image
+         
+        You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).
+        You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. 
+        DO NOT allocate another 2D matrix and do the rotation.
+
+        Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+        Output: [[7,4,1],[8,5,2],[9,6,3]]
+
+        Input: matrix = [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]
+        Output: [[15,13,2,5],[14,3,4,1],[12,6,8,9],[16,7,10,11]]
+         
+        */
+
+        public void Rotate(int[][] matrix)
+        {
+
+            int n = matrix.Length;
+
+            for (int i = 0; i < n / 2 + n % 2; i++)
+            {
+                for (int j = 0; j < n / 2; j++)
+                {
+
+                    int temp = matrix[n - 1 - j][i];
+                    matrix[n - 1 - j][i] = matrix[n - 1 - i][n - 1 - j];
+                    matrix[n - 1 - i][n - 1 - j] = matrix[j][n - 1 - i];
+                    matrix[j][n - 1 - i] = matrix[i][j];
+                    matrix[i][j] = temp;
+                }
+            }
+        }
+
+        /*  Merge Intervals
+         Input: intervals = [[1,3],[2,6],[8,10],[15,18]]
+         Output: [[1,6],[8,10],[15,18]]
+         Explanation: Since intervals [1,3] and [2,6] overlap, merge them into [1,6].
+
+        Input: intervals = [[1,4],[0,2],[3,5]]
+        Output: [[0,5]]
+
+         */
+
+        public int[][] Merge(int[][] intervals)
+        {
+
+            int[][] output = new int[intervals.Length][];
+            int k = 0;
+
+            for (int i = 0; i < intervals.Length - 1; i++)
+            {
+
+                if (intervals[i][1] >= intervals[i + 1][0])
+                {
+
+                    output[k][0] = intervals[i][0];
+                    output[k][1] = intervals[i + 1][1];
+                    i++;
+                    k++;
+                }
+                else
+                {
+                    output[k][0] = intervals[i][0];
+                    output[k][1] = intervals[i][1];
+                    k++;
+                }
+            }
+            return output;
+        }
+
+        /*  Find the Duplicate Number
+         
+        Input: nums = [1,3,4,2,2]
+        Output: 2
+
+        Input: nums = [3,1,3,4,2]
+        Output: 3
+
+         */
+
+        public int FindDuplicate(int[] nums)
+        {
+
+            int slow_pointer = nums[0];
+            int fast_pointer = nums[0];
+            Console.WriteLine("slow " + slow_pointer + " fast " + fast_pointer);
+            do
+            {
+
+                slow_pointer = nums[slow_pointer];
+                fast_pointer = nums[nums[fast_pointer]];
+
+                Console.WriteLine("slow " + slow_pointer + " fast " + fast_pointer);
+            } while (slow_pointer != fast_pointer);
+
+            slow_pointer = nums[0];
+
+            Console.WriteLine("Phase 2 --slow " + slow_pointer + " fast " + fast_pointer);
+            while (slow_pointer != fast_pointer)
+            {
+                slow_pointer = nums[slow_pointer];
+                fast_pointer = nums[fast_pointer];
+
+                Console.WriteLine("slow " + slow_pointer + " fast " + fast_pointer);
+            }
+
+            return slow_pointer;
+
+        }
+
+        /* Pow(x, n)
+         
+        Implement pow(x, n), which calculates x raised to the power n (i.e., x^n).
+
+         */
+
+        public double MyPow(double x, int n)
+        {
+
+            double product = 1;
+            long nn = n; // Long is larger than double. it will take care of the owerflow when -n is converted to +n;
+
+            if (nn < 0) nn = -1 * nn; //convert -ve n to positive
+
+            while (nn > 0)
+            {
+
+                //if power is odd x^n = x * x^(n-1)
+                if (nn % 2 != 0)
+                {
+                    product *= x;
+                    nn--;
+                }
+                else
+                {
+
+                    //if power is even x^n = (x * x)^(n/2)
+                    x = x * x;
+                    nn = nn / 2;
+                }
+
+            }
+
+            if (n < 0)
+            {
+                product = (double)1 / (double)product;
+            }
+
+            return product;
+        }
+
+        /*
+
+        */
     }
 }
